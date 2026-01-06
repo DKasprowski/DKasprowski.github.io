5f6c42b34e7a0cd46da265f5e04a97fac3496fbc
DKasprowski <92478624+DKasprowski@users.noreply.github.com>
2026-01-06 16:31:20 +0000

    Updated Math2049

--- MATH2049/static/customisation.js
+++ MATH2049/static/customisation.js
@@ -5,9 +5,8 @@ class Setting {
         this.default_value = default_value;
         this._value = default_value;
 
-        const id = `display-options-${name}`;
-        this.input = document.getElementById(id);
-        this.output = document.querySelector(`output[for="${id}"]`);
+        this.input = document.querySelector(`#display-options [name="${name}"]`);
+        this.output = document.querySelector(`output[for="display-options-${name}"]`);
 
         const change = () => {
             this.value = this.filter_value(this.input_value);
@@ -50,6 +49,10 @@ class Setting {
         this.output.textContent = this.display_value;
     }
 
+    update_page() {
+        document.documentElement.style.setProperty(`--${this.name}`, this.css_value);
+    }
+
     get value() {
         return this._value;
     }
@@ -63,6 +66,18 @@ class Setting {
     }
 }
 
+class CssSetting extends Setting {
+    constructor() {
+        super(...arguments);
+
+        this.style = document.createElement('style');
+        document.head.append(this.style);
+    }
+    update_page() {
+        this.style.textContent = this.value;
+    }
+}
+
 class AngleSetting extends Setting {
     get css_value() {
         return `${this._value}turn`;
@@ -71,6 +86,16 @@ class AngleSetting extends Setting {
     get display_value() {
         const t = this._value;
         return `${t} ${t==1 ? _('turn') : _('turns')}`
+    }
+}
+
+class NumberSetting extends Setting {
+    get css_value() {
+        return this.value.toString();
+    }
+
+    get display_value() {
+        return this.value.toString();
     }
 }
 
@@ -141,20 +166,26 @@ class Customiser {
     add_setting(name, kind, default_value) {
         const kinds = {
             'exponentialpercentage': ExponentialPercentageSetting,
+            'number': NumberSetting,
             'percentage': PercentageSetting,
             'boolean': BooleanSetting,
             'angle': AngleSetting,
             'colour': Setting,
             'text': Setting,
+            'css': CssSetting,
         }
         this.settings[name] = new kinds[kind](this, name, default_value);
     }
 
     constructor() {
         this.settings = {};
+        this.form = document.querySelector('#display-options > form');
+        this.form.addEventListener('submit', e => e.preventDefault());
+
         this.localStorage_key = 'chirun-theme-customization';
 
         this.add_setting('font-scale', 'exponentialpercentage', 100);
+        this.add_setting('font-weight', 'number', 400);
         this.add_setting('spacing-factor', 'exponentialpercentage', 100);
         this.add_setting('font-family', 'text', 'var(--sans-serif-font)');
         this.add_setting('colour-scheme', 'text', 'auto');
@@ -173,14 +204,19 @@ class Customiser {
 
         this.add_setting('invert-images', 'boolean', true);
 
-        const display_options_form = document.getElementById('display-options');
+        this.add_setting('custom-css', 'css', '');
+
+        const display_options_dialog = document.getElementById('display-options');
         const toggle_buttons = document.querySelectorAll('button[aria-controls="display-options"]');
         for(let button of toggle_buttons) {
             button.addEventListener('click', () => {
-                display_options_form.classList.toggle('show');
-                const displayed = display_options_form.classList.contains('show');
-
-                toggle_buttons.forEach(b => b.setAttribute('aria-expanded', displayed));
+                display_options_dialog.showModal();
+            });
+        }
+        const close_buttons = display_options_dialog.querySelectorAll('button[aria-controls="display-options"]')
+        for(let button of close_buttons) {
+            button.addEventListener('click', () => {
+                display_options_dialog.close();
             });
         }
 
@@ -217,11 +253,12 @@ class Customiser {
     }
 
     update_page() {
-        Object.entries(this.settings).forEach(([k,{css_value}]) => {
-            document.documentElement.style.setProperty(`--${k}`, css_value);
+        Object.values(this.settings).forEach((setting) => {
+            setting.update_page();
         });
         document.body.dataset.colourScheme = this.settings['colour-scheme'].value;
         document.body.dataset.invertImages = this.settings['invert-images'].value;
+        document.body.classList.toggle('big-text', this.settings['font-scale'].value >= 200);
     }
 
     reset() {
@@ -232,3 +269,256 @@ class Customiser {
 }
 
 const customiser = new Customiser();
+
+function treeview(element) {
+    let search = '';
+
+    element.addEventListener('click', e => {
+        if(!e.target.classList.contains('toc-marker')) {
+            return;
+        }
+        const a = e.target.parentElement;
+        a.ariaExpanded = a.ariaExpanded == 'true' ? 'false' : 'true';
+        e.preventDefault();
+    });
+
+    const all_items = [];
+
+    function get_children(el) {
+        const group = el.querySelector('[role="group"]');
+        return group ? group.children : [];
+    }
+
+    function find_all_items(el) {
+        const treeitem = el.querySelector('[role="treeitem"]');
+
+        if(!treeitem) {
+            return;
+        }
+
+        all_items.push(treeitem);
+
+        if(treeitem.ariaExpanded == 'true') {
+            for(let child of get_children(el)) {
+                find_all_items(child);
+            }
+        }
+    }
+    find_all_items(element);
+
+
+    element.addEventListener('keydown', e => {
+        const modifier = e.altKey || e.ctrlKey || e.metaKey || e.shiftKey;
+
+        const focused = document.activeElement;
+
+        // find the <li> element for the focused item.
+        let el = focused;
+        while(el.parentElement && el.parentElement.role != 'group' && el.parentElement.role != 'tree') {
+            el = el.parentElement;
+        }
+        if(!el.parentElement) {
+            return;
+        }
+        const siblings = Array.from(el.parentElement.children);
+        const i = siblings.indexOf(el);
+
+        function next_item(el) {
+            const treeitem = el.querySelector('[role="treeitem"]');
+            const children = get_children(el);
+            if(children.length > 0 && treeitem.ariaExpanded == 'true' ) {
+                return children[0];
+            } else {
+                return next_sibling(el);
+            }
+        }
+
+        function next_sibling(el) {
+            const siblings = Array.from(el.parentElement.children);
+            const i = siblings.indexOf(el);
+            if(i == siblings.length-1) {
+                if(el.parentElement.role == 'tree') {
+                    return;
+                } else {
+                    return next_sibling(el.parentElement.parentElement);
+                }
+            } else {
+                return siblings[i+1];
+            }
+        }
+
+        function last_item(el) {
+            const treeitem = el.querySelector('[role="treeitem"]');
+            const children = get_children(el);
+            if(children.length > 0 && treeitem.ariaExpanded == 'true' ) {
+                return last_item(children[children.length-1]);
+            } else {
+                return el;
+            }
+        }
+
+        function previous_item(el) {
+            const siblings = Array.from(el.parentElement.children);
+            const i = siblings.indexOf(el);
+            if(i == 0) {
+                if(el.parentElement.role == 'tree') {
+                    return;
+                } else {
+                    return el.parentElement.parentElement;
+                }
+            } else {
+                return last_item(siblings[i-1]);
+            }
+        }
+
+        function focus_item(el, clear_search) {
+            if(clear_search !== false) {
+                search = '';
+            }
+            if(!el) {
+                return;
+            }
+            el.querySelector('[role="treeitem"]').focus();
+        }
+
+        const handlers = {
+            'ArrowLeft': () => {
+                const treeitem = el.querySelector('[role="treeitem"]');
+                if(treeitem.ariaExpanded == 'true') {
+                    treeitem.ariaExpanded = 'false';
+                    return;
+                }
+                if(el.role == 'tree') {
+                    return;
+                }
+                focus_item(el.parentElement.parentElement);
+            },
+            'ArrowRight': () => {
+                const treeitem = el.querySelector('[role="treeitem"]');
+                const children = get_children(el);
+                if(children.length > 0) {
+                    if(treeitem.ariaExpanded == 'false') {
+                        treeitem.ariaExpanded = 'true';
+                        return;
+                    }
+                    focus_item(children[0]);
+                }
+            },
+            'ArrowUp': () => {
+                focus_item(previous_item(el));
+            },
+            'ArrowDown': () => {
+                focus_item(next_item(el));
+            },
+            'Home': () => {
+                const first_item = element.querySelector('[role="treeitem"]');
+                if(!first_item) {
+                    return;
+                }
+                focus_item(first_item.parentElement);
+            },
+            'End': () => {
+                const last_item = all_items[all_items.length-1];
+                if(!last_item) {
+                    return;
+                }
+                focus_item(last_item.parentElement);
+            },
+            'Backspace': () => {
+                search = search.slice(0, search.length-1);
+            }
+        }
+
+        if(handlers[e.key]) {
+            handlers[e.key]();
+            e.preventDefault();
+        } else if(e.key.length==1 && !modifier) {
+            let j = all_items.indexOf(focused);
+            const cycled_items = all_items.slice(j+1).concat(all_items.slice(0,j));
+            search += e.key.toLowerCase();
+            const item = cycled_items.find(item => item.textContent.toLowerCase().includes(search));
+            if(item) {
+                focus_item(item.parentElement, false);
+                e.preventDefault();
+            } else {
+                search = '';
+            }
+        }
+
+    });
+
+    let update_visibility_timeout;
+    let last_update = 0;
+    const max_update_interval = 100;
+    function update_visibility() {
+        const t = new Date();
+        const dt = t - last_update;
+        if(dt < max_update_interval) {
+            if(!update_visibility_timeout) {
+                update_visibility_timeout = setTimeout(() => {
+                    update_visibility();
+                }, max_update_interval - dt);
+            }
+            return;
+        }
+
+        update_visibility_timeout = null;
+        last_update = t;
+
+        function set_not_visible(item) {
+            item.classList.remove('visible');
+        }
+
+        function set_visible(item, level) {
+            level = level || 0;
+            item.classList.add('visible');
+            let up = item;
+            for(let i=0;i<3;i++) {
+                up = up.parentElement;
+                if(!up) {
+                    return;
+                }
+            }
+            if(up.classList.contains('toc-header')) {
+                set_visible(up.querySelector('[role="treeitem"]'), level + 1);
+            }
+        }
+
+        const items = element.querySelectorAll('[role="treeitem"]');
+        for(let i=0;i<items.length;i++) {
+            const item = items[i];
+
+            const header = document.getElementById(item.getAttribute('href').slice(1));
+            const {top, bottom} = header.getBoundingClientRect();
+            const visibility = bottom < 0 ? 'above' : top > window.innerHeight ? 'below' : 'visible';
+
+            item.dataset.visibility = visibility;
+
+            item.tabIndex = -1;
+            switch(visibility) {
+                case 'above':
+                    set_not_visible(item);
+                    break;
+                case 'visible':
+                    set_visible(item);
+                    break;
+                case 'below':
+                    set_not_visible(item);
+                    if(i > 0 && items[i-1].dataset.visibility != 'below') {
+                        set_visible(items[i-1], 100);
+                    }
+                    break;
+            }
+        }
+
+        const visible_items = all_items.filter(item => item.classList.contains('visible'));
+        if(visible_items.length) {
+            visible_items[visible_items.length-1].tabIndex = 0;
+        }
+    }
+    update_visibility();
+    document.addEventListener('scroll', update_visibility);
+}
+
+
+Array.from(document.querySelectorAll('body > main > #sidebar .table-of-contents')).forEach(treeview);
